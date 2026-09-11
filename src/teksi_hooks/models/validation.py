b003add54820d6a598bc8021289c8869123cd918
Cyril Meder <c.meder@wapa.ch>
2026-09-11 10:09:50 +0200

    import utc

--- src/teksi_hooks/models/validation.py
+++ src/teksi_hooks/models/validation.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass, field
-from datetime import datetime
+from datetime import UTC, datetime
 from enum import StrEnum
 from typing import Any
 from collections.abc import Mapping
